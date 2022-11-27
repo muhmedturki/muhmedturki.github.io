e1ed6d87df9380fc6017220151de1b657f6ffa64
muhmedturki <118129453+muhmedturki@users.noreply.github.com>
2022-11-27 23:57:12 +0300

    add.

--- project/urls.py
+++ project/urls.py
@@ -17,14 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
+
 urlpatterns = [
     path('grappelli/',include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
     path('products/',include('products.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
