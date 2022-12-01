c063e9c57fc3aea7470819025fe1c791f771e5f5
muhmedturki <118129453+muhmedturki@users.noreply.github.com>
2022-12-01 20:51:01 +0300

    add.

--- project/project/settings.py
+++ project/project/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL= '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#django_heroku.settings(locals())
+
 STATIC_DIRS =[
 os.path.join(BASE_DIR, "project/static"),
 ]
-
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -148,4 +148,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
