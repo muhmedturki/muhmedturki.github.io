62745ad8368ee9d8594980abf37675b5c536b832
muhmedturki <118129453+muhmedturki@users.noreply.github.com>
2022-12-01 21:41:39 +0300

    add.

--- project/project/project/settings.py
+++ project/project/project/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL= '/static/'
-STATICFILES_DIRS=[ 
-    os.path.join(BASE_DIR, 'Project/static'),
-]
+#STATICFILES_DIRS=[ 
+ #   os.path.join(BASE_DIR, 'Project/static'),
+#]
 django_heroku.settings(locals())
 
 # Default primary key field type
