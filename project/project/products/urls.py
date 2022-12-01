7fa300de8ce5d2eb3b42e509900f4d6220370ed1
muhmedturki <118129453+muhmedturki@users.noreply.github.com>
2022-12-01 21:48:17 +0300

    add.

--- project/project/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('products', views.products, name='products'),
-    path('product', views.product, name='product'),
-]
-
